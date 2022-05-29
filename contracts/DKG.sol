0576ca260f57caa08762ace95e0eefc64796eba0
Alon Muroch <alonmuroch@gmail.com>
2022-05-29 20:34:53 +0300

    prev

---
+++ contracts/DKG.sol
@@ -0,0 +1,73 @@
+// File: contracts/ISSVRegistry.sol
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.2;
+
+import "./ISSVRegistry.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract IDKG {
+    using ECDSA for bytes32;
+
+    mapping(bytes => bytes) depositSignature;
+    ISSVRegistry ssvRegistry;
+
+    /**
+     * @dev A callback for a dkg run
+     * @param operatorIds Operators SSV ID
+     * @param signatures validation signature which result from DKG to validate the operator actually generated them
+     * @param encryptedShares Operator shares for the validator
+     * @param sharesPublicKeys the PK for the encrypted shares
+     * @param setSize the number of operators participating in the DKG
+     * @param withdrawalCredentials the provided credentials as input to the DKG
+     * @param depositSignature a valid signature to be used to deposit the eth2 validator
+     * @param validatorPubKey the validator's pub key
+     */
+    function registerValidator(
+        uint256[] calldata operatorIds,
+        bytes[]  calldata signatures,
+        bytes[] calldata encryptedShares,
+        bytes[] calldata sharesPublicKeys,
+        uint8 calldata setSize,
+        bytes calldata withdrawalCredentials,
+        bytes calldata depositSignature,
+        bytes calldata validatorPubKey
+    ) external {
+        // verify signature
+        for (uint8 index = 0; index < setSize; ++index) {
+            res = keccak256(
+                abi.encode(
+                    signatures[index],
+                    address,
+                    encryptedShares[index],
+                    setSize,
+                    withdrawalCredentials,
+                    validatorPubKey
+                )
+            )
+            .toEthSignedMessageHash()
+            .recover(signatures[index]) == ssvRegistry.operatorAddressByID(operatorIds[index]);
+
+            require(
+               res,
+                "signature invalid"
+            );
+        }
+
+        // register validator
+        ssvRegistry.registerValidator(
+            address(this),
+            validatorPubKey,
+            operatorIds,
+            sharesPublicKeys,
+            encryptedShares,
+            false
+        );
+
+        // save validator info for deposit
+        depositSignature[validatorPubKey] = depositSignature;
+    }
+
+    function depositDataForValidator(bytes validatorPubKey) external returns (bytes) {
+        return depositSignature[validatorPubKey];
+    }
+}
